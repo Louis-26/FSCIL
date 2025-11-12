f15e8213972a21da49311096003719336ce48adc
Yi Lu <121090386@link.cuhk.edu.cn>
2025-11-12 11:41:18 -0500

    update

--- git_script/git_push_branch.sh
+++ git_script/git_push_branch.sh
@@ -1,5 +1,5 @@
 # uncomment if it is in linux, and need to convert dos to unix
-sed -i 's/\r$//' git_script/git_push.sh
+#sed -i 's/\r$//' git_script/git_push.sh
 read -p "Enter the name of your feature branch: " BRANCH_NAME
 
 git add .
